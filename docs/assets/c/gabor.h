1bf3770ea24efeff7d67f5e26a874a567de87605
Andreas Aeschlimann <andreas.aeschlimann@appvision.ch>
2018-06-11 01:29:50 +0200

    Updated web application

--- docs/assets/c/gabor.h
+++ docs/assets/c/gabor.h
@@ -1,6 +1,7 @@
 #include <complex.h>
+#include <emscripten/emscripten.h>
 
-void filter2(float complex *gw, int n, float xi, float sigma, float lambda, float theta);
-void normalizedFilter2(float complex *gw, int n, float xi, float sigma, float lambda, float theta);
-void translate2(float complex *f, float complex *fShift, int n, int hShift, int vShift);
-void mirrorYCoordinate(float complex *f, float complex *f2, int n);
+void _filter2(float complex *gw, int n, float xi, float sigma, float lambda, float theta);
+void _normalizedFilter2(float complex *gw, int n, float xi, float sigma, float lambda, float theta);
+void _translate2(float complex *f, float complex *fShift, int n, int hShift, int vShift);
+void _mirrorYCoordinate(float complex *f, float complex *f2, int n);
---
+++ docs/assets/js/normalizedFilter2.js
@@ -0,0 +1,69 @@
+"use strict";
+
+var Module = {
+    locateFile: function (s) {
+        return '../c/' + s;
+    },
+    onRuntimeInitialized: function() {
+        normalizedFilter2();
+    }
+};
+
+importScripts("../c/main.js");
+
+var n = 0;
+var xi =  0;
+var sigma = 0;
+var lambda = 0;
+var theta = 0;
+var amount = 0;
+
+var onmessage = function(messageEvent) {
+    n = messageEvent.data.n;
+    xi =  messageEvent.data.xi;
+    sigma = messageEvent.data.sigma;
+    lambda = messageEvent.data.lambda;
+    theta = messageEvent.data.theta;
+}
+
+var normalizedFilter2 = function() {
+
+    // Check size
+    if (n % 1 !== 0 || Math.log2(n) % 1 !== 0) {
+        console.error("Input data length is not power of 2.");
+        postMessage({});
+        return;
+    }
+
+    // Generate float arrays
+    var gReal = new Float32Array(n*n); // filter
+    var gImag = new Float32Array(n*n); // filter
+
+    // Call c code
+    try {
+        const buffer1 = Module._malloc(gReal.length * gReal.BYTES_PER_ELEMENT);
+        const buffer2 = Module._malloc(gImag.length * gImag.BYTES_PER_ELEMENT);
+        Module.HEAPF32.set(gReal, buffer1 >> 2);
+        Module.HEAPF32.set(gImag, buffer2 >> 2);
+
+        Module.ccall(
+            "normalizedFilter2",
+            null,
+            ["number", "number", "number", "number", "number", "number", "number"],
+            [buffer1, buffer2, n, xi, sigma, lambda, theta]
+        );
+
+        gReal = new Float32Array(Module.HEAPF32.buffer, buffer1, gReal.length);
+        gImag = new Float32Array(Module.HEAPF32.buffer, buffer2, gImag.length);
+        Module._free(buffer1);
+        Module._free(buffer2);
+
+    } catch (e) {
+        console.error(e);
+    }
+
+    console.log(gReal);
+
+    postMessage({gReal: gReal, gImag: gImag});
+
+}
