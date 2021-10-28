4a9eadd44a955fadf2a99331cfac3330b65d0550
Andreas Aeschlimann <andreas.aeschlimann@appvision.ch>
2021-10-28 22:30:46 +0200

    Update wasm files

--- src/assets/c/compile.sh
+++ src/assets/c/compile.sh
@@ -3,4 +3,4 @@
 # cd /opt/emsdk/
 # source ./emsdk_env.sh
 
-emcc -O3 -s WASM=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']" -s ALLOW_MEMORY_GROWTH=1 -s "EXPORTED_FUNCTIONS=['_malloc', '_free']" -o main.js main.c fourier.c gabor.c
+emcc -O3 -s WASM=1 -s "EXPORTED_RUNTIME_METHODS=['ccall']" -s ALLOW_MEMORY_GROWTH=1 -s "EXPORTED_FUNCTIONS=['_malloc', '_free']" -o main.js main.c fourier.c gabor.c
